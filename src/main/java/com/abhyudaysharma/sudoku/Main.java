4b2447b17d5903a2aaa429b8814ff85d9af90e1e
Abhyudaya Sharma <sharmaabhyudaya@gmail.com>
2019-08-28 18:18:38 +0530

    Initial commit

---
+++ src/main/java/com/abhyudaysharma/sudoku/Main.java
@@ -0,0 +1,7 @@
+package com.abhyudaysharma.sudoku;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
---
+++ src/test/java/com/abhyudaysharma/sudoku/AppTest.java
@@ -0,0 +1,15 @@
+package com.abhyudaysharma.sudoku;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AppTest {
+    /**
+     * Sample test for the application.
+     */
+    @Test
+    void test() {
+        assertTrue(true);
+    }
+}
