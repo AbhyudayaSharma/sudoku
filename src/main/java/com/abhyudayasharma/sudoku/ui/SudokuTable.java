dd0e29afe86494917d7ee092d2202c9250868a88
Abhyudaya Sharma <sharmaabhyudaya@gmail.com>
2019-09-05 16:07:54 +0530

    Add borders to the sudoku
    Not perfect but works https://imgur.com/a/o8fRSFJ

--- src/main/java/com/abhyudayasharma/sudoku/ui/SudokuTable.java
+++ src/main/java/com/abhyudayasharma/sudoku/ui/SudokuTable.java
@@ -1,13 +1,16 @@
 package com.abhyudayasharma.sudoku.ui;
 
 import com.abhyudayasharma.sudoku.Sudoku;
+import com.abhyudayasharma.sudoku.SudokuBoard;
 
 import javax.swing.BorderFactory;
+import javax.swing.JComponent;
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
 import javax.swing.SwingConstants;
 import javax.swing.table.DefaultTableCellRenderer;
 import java.awt.Color;
+import java.awt.Component;
 import java.net.URI;
 
 public class SudokuTable extends JTable {
@@ -24,8 +27,39 @@ public class SudokuTable extends JTable {
         tableHeader = null;
 
         setColumnCellEditors();
-        var centeredRenderer = new DefaultTableCellRenderer();
+        var centeredRenderer = new DefaultTableCellRenderer() {
+            private final int sqrt = (int) Math.rint(Math.sqrt(SudokuBoard.SIZE));
+            private final Color borderColor = Color.BLACK;
+
+            @Override
+            public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
+                                                           boolean hasFocus, int row, int column) {
+                final var component = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+                var border = BorderFactory.createEmptyBorder();
+
+                final var borderThickness = 3;
+                if (row % sqrt == sqrt - 1 && row != SudokuBoard.SIZE - 1) {
+                    border = BorderFactory.createCompoundBorder(border,
+                        BorderFactory.createMatteBorder(0, 0, borderThickness, 0, borderColor));
+                }
+
+                if (column % sqrt == sqrt - 1 && column != SudokuBoard.SIZE - 1) {
+                    border = BorderFactory.createCompoundBorder(border,
+                        BorderFactory.createMatteBorder(0, 0, 0, borderThickness, borderColor));
+                }
+
+                if (component instanceof JComponent) {
+                    ((JComponent) component).setBorder(border);
+                } else {
+                    throw new IllegalArgumentException("component should be a JComponent");
+                }
+
+                return component;
+            }
+        };
         centeredRenderer.setHorizontalAlignment(SwingConstants.CENTER);
+        centeredRenderer.setBorder(BorderFactory.createLineBorder(Color.BLUE, 10));
+
         setDefaultRenderer(Object.class, centeredRenderer);
         setFont(Sudoku.BOARD_FONT);
         setBorder(BorderFactory.createLineBorder(Color.BLACK, 2, false));
